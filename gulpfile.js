d26a675b986f7cf4ced51724403ed04f3040c5c5
HeadMad <kov.ps@ya.ru>
2018-09-05 16:13:43 +0800

    edit styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp     = require('gulp');
+
+// Функция ленивой загрузки задач
+function requireTask (taskName, path, options)
+{
+	options.taskName = taskName;
+	gulp.task(taskName, function(callback){
+		let task = require(path)
+		.call(this, options);
+		return task(callback);
+	});
+}
+
+requireTask('styles', './tasks/styles', {
+	src: ['./assets/css/*.css'],
+	file: 'styles.css',
+	dest: './public/css'
+});
--- tasks/styles.js
+++ tasks/styles.js
@@ -1,34 +1,25 @@
 
-const gulp =        require('gulp');
-const $ =           require('gulp-load-plugins')();
+const gulp = require('gulp');
+const $    = require('gulp-load-plugins')();
 // const browserSync = require('browser-sync');
 
 module.exports = function(options) {
 
 	return function (callback) {
 			var sourcemaps = require('gulp-sourcemaps');
+
+            // массив плагинов для postcss
 			var plugins = [
-			// require('precss')(),
-			// require('postcss-smart-import')(),
-			require('postcss-short')(),
-			require('postcss-cssnext')({
-				compress: true
-			}),
-			require('css-mqpacker')({sort: false}),
-			// require('cssnano')({
-			// 	core: false,
-			// 	autoprefixer: false,
-			// })
+    			require('postcss-short')(),
+    			require('postcss-cssnext')({compress: true}),
+    			// require('css-mqpacker')({sort: false}),
 			];
 
-			gulp.src(options.src)
+			return gulp.src(options.src)
 			.pipe(sourcemaps.init())
 			.pipe($.concat(options.file))
 			.pipe($.postcss(plugins))
-			// .pipe($.csscomb())
 			.pipe(sourcemaps.write())
 			.pipe(gulp.dest(options.dest));
-			// .pipe(browserSync.reload({stream: true}))
-			callback();
 	};
 };
