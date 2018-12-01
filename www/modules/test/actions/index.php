c1e929167c622fd7f2b8388a6a27af4f7b5190d5
HeadMad <kov.ps@ya.ru>
2018-12-02 01:18:28 +0800

    final comit

--- www/modules/test/actions/index.php
+++ www/modules/test/actions/index.php
@@ -10,7 +10,7 @@
 	
 	// Добавляем кешированный контент
 	$content = cache('bufer', 'modules/test/cache/content');
-	$vars['content'] = $content('test/content/index', ['h1' => 'Ntcns!']);
+	$vars['content'] = $content('test/content/index', ['h1' => 'Тест статики и и кеша']);
 
 	// Выводим код главной страницы
 	$layout = bufer('test/layout', $vars);
